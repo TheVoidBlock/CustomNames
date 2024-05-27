20dd0dacb9216bcecfd87c6d6ad2f5852dc90ef6
The Void Block <72237088+TheVoidBlock@users.noreply.github.com>
2024-05-26 19:18:21 -0600

    Delete src/main/java/com/thevoidblock/customnames/gui directory

--- src/main/java/com/thevoidblock/customnames/gui/ModConfigScreen.java
+++
@@ -1,9 +0,0 @@
-//package com.thevoidblock.customnames.gui;
-//
-//import net.minecraft.client.gui.screen.Screen;
-//
-//public class ModConfigScreen {
-//    public Screen getConfigScreen() {
-//
-//    }
-//}
