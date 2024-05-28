43a53be8540bceda07f282f7424900f30878af36
void <wordhh1@gmail.com>
2024-05-28 01:47:43 -0600

    Updated method mixin for 1.20.6

--- src/main/java/com/thevoidblock/customnames/mixin/PlayerEntityRendererMixin.java
+++ src/main/java/com/thevoidblock/customnames/mixin/PlayerEntityRendererMixin.java
@@ -15,8 +15,8 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 public class PlayerEntityRendererMixin {
 
     @ModifyArgs(
-            method = "renderLabelIfPresent(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/LivingEntityRenderer;renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", ordinal = 1)
+            method = "Lnet/minecraft/client/render/entity/PlayerEntityRenderer;renderLabelIfPresent(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/LivingEntityRenderer;renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V", ordinal = 1)
     )
     protected void renderLabelIfPresent(Args args) {
 
