03cd7d3178854faa5bbb63a196d2ff021d932fe2
void <wordhh1@gmail.com>
2024-07-02 22:10:41 -0600

    Changed mod initialization to use ClientModInitializer instead of ModInitializer

--- src/main/java/com/thevoidblock/customnames/CustomNames.java
+++ src/main/java/com/thevoidblock/customnames/CustomNames.java
@@ -2,19 +2,19 @@ package com.thevoidblock.customnames;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CustomNames implements ModInitializer {
+public class CustomNames implements ClientModInitializer {
 
     public static final String MOD_ID = "customnames";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
 
         AutoConfig.register(CustomNamesConfig.class, JanksonConfigSerializer::new);
 
