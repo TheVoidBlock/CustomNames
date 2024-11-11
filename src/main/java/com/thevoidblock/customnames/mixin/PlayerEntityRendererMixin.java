bf6262cb763a4c7132341e22ce65b7a1305702fa
void <wordhh1@gmail.com>
2024-11-11 15:20:32 -0700

    Remove unnecessary qualified reference to 'renderLabelIfPresent'

--- src/main/java/com/thevoidblock/customnames/mixin/PlayerEntityRendererMixin.java
+++ src/main/java/com/thevoidblock/customnames/mixin/PlayerEntityRendererMixin.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 public class PlayerEntityRendererMixin {
 
     @ModifyArgs(
-            method = "Lnet/minecraft/client/render/entity/PlayerEntityRenderer;renderLabelIfPresent(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V",
+            method = "renderLabelIfPresent(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/LivingEntityRenderer;renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V", ordinal = 1)
     )
     protected void renderLabelIfPresent(Args args) {
