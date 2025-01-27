d26f3eeeb43065d9915e9c51c8f58d37b45b1bd7
void <wordhh1@gmail.com>
2025-01-27 00:41:20 -0700

    Modify getDisplayName method instead of renderLabelIfPresent method. This will make it so it modifies the name for selectors in text too

--- src/main/java/com/thevoidblock/customnames/mixin/PlayerEntityRendererMixin.java
+++ src/main/java/com/thevoidblock/customnames/mixin/PlayerEntityRendererMixin.java
@@ -3,30 +3,28 @@ package com.thevoidblock.customnames.mixin;
 import com.thevoidblock.customnames.CustomNames;
 import com.thevoidblock.customnames.CustomNamesConfig;
 import me.shedaniel.autoconfig.AutoConfig;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.client.render.entity.PlayerEntityRenderer;
-import net.minecraft.text.MutableText;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
-@Mixin(PlayerEntityRenderer.class)
+@Mixin(PlayerEntity.class)
 public class PlayerEntityRendererMixin {
 
     @ModifyArgs(
-            method = "renderLabelIfPresent(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/LivingEntityRenderer;renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IF)V", ordinal = 1)
+            method = "Lnet/minecraft/entity/player/PlayerEntity;getDisplayName()Lnet/minecraft/text/Text;",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/scoreboard/Team;decorateName(Lnet/minecraft/scoreboard/AbstractTeam;Lnet/minecraft/text/Text;)Lnet/minecraft/text/MutableText;")
     )
     protected void renderLabelIfPresent(Args args) {
 
         CustomNamesConfig config = AutoConfig.getConfigHolder(CustomNamesConfig.class).getConfig();
 
         if(config.enabled) {
-            Text name = ((AbstractClientPlayerEntity)args.get(0)).getName();
+            Text name = args.get(1);
             if(CustomNames.checkNameModification(config, name))
-                args.set(1, CustomNames.getAppliedName(config, (MutableText) name));
+                args.set(1, CustomNames.getAppliedName(config, name));
         }
 
     }
